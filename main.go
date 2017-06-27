ed7270176f30089dc4cf68a8bee15e954d8ec0b3
David Gee <david.john.gee@gmail.com>
2017-06-27 08:12:31 +0200

    Added somework

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 const (
+	// We need another constant..obviously.
+	urgentconstant1 = 1
 
 	// Create constant using 'iota' and start from 1 (iota starts from zero)
 	constant1 = iota + 1
@@ -16,9 +18,13 @@ func main() {
 	fmt.Println("Constant 1: ", constant1)
 	fmt.Println("Constant 2: ", constant2)
 
+	// Needed this for some reason
+	fmt.Println("Urgent Constant: ", urgentconstant1)
+
 	// Output
 	// -------
 	// Constant 1:  2
 	// Constant 2:  3
+	// Urgent Constant:  1
 	//
 }
